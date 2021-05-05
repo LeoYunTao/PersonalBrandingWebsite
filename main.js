35f176977431254d29f08ab3275179ff7fdfd138
Leo Yun Tao <39025657+LeoYunTao@users.noreply.github.com>
2021-05-05 09:34:18 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-let nav = document.querySelector("nav .navbar-nav");
+// let nav = document.querySelector("nav .navbar-nav");
 
-nav.addEventListener("click", event => {
-    nav.querySelector(".active").className = "nav-link";
-    event.target.className += " active";
-});
+// nav.addEventListener("click", event => {
+//     nav.querySelector(".active").className = "nav-link";
+//     event.target.className += " active";
+// });
 
-//TODO make the script detect the section automatically then change the class name
+// //TODO make the script detect the section automatically then change the class name
